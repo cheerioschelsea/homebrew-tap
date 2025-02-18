eb97a8a1d6d0b30c3c7a4fa41972777176e2ce49
cheerioschelsea <37007731+cheerioschelsea@users.noreply.github.com>
2025-02-17 20:08:04 -0500

    Update ralibretro.rb

--- Casks/ralibretro.rb
+++ Casks/ralibretro.rb
@@ -1,7 +1,7 @@
 cask "ralibretro" do
   version "1.8.0-macOS"
   sha256 "d0db1a067f03edf472f0d24510006164020a863fd25a4c06f61390fb3272f240"
-  url "https://github.com/cheerioschelsea/RALibretro/releases/download/v#{version}/RALibretro-#{version}.zip"
+  url "https://github.com/cheerioschelsea/RALibretro/releases/download/1.8.0-macOS/RALibretro-1.8.0-macOS.zip"
   name "RALibretro"
   desc "emulator for developing retro achievements"
   homepage "https://github.com/RetroAchievements/RALibretro"
